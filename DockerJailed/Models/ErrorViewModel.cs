adb7edd4dc37d2ce7211e06c5758c4e4eeab1cfd
sigged <sderdeyn@gmail.com>
2018-10-02 23:51:41 +0200

    Add project files.

---
+++ DockerJailed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerJailed.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
